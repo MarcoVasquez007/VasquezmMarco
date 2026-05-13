287b1d003c62ef89e58920096ce5b8808c564703
Laboratorio <laboratorio@udla-ec.int>
2026-05-13 15:14:47 -0500

    Ejercicio sizeof

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 int main(){
-    char vocales[]={'a','e','i','o','u'};
    
-    char *ptr, *aux;
-    ptr=&vocales[0];
-    aux=&vocales[4];
-    *aux='x';
-   
-    for (int i=0;i<5;i++){
-        printf("vocales[%d]%c ",i,vocales[i]);
-        printf("%c\n",*(ptr++));
-    }
+    char tab[] = {'a', 'e', 'i', 'o', 'u'};
+
+   int i = sizeof(tab);
+    printf("%d", i);
 }
