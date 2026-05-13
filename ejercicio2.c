9a1bdc4bf0b3f149db581c01a83dc2ccc6fb219f
Laboratorio <laboratorio@udla-ec.int>
2026-05-13 14:46:16 -0500

    Segundo commit

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,14 +1,14 @@
+ 
 #include <stdio.h>
-
-int main(){
-    int vec[5] = {2, 4, 6, 8, 10};
+ 
+int main() {
+    int vec[5]={2,4,6,8,10};
     int *ptr;
     ptr = &vec[0];
-    printf("%p", ptr);
-    for (int i = 0; i < 5; i++)
-    {
-    printf("%d", vec[i]);
-    printf("%d \n", *(ptr + i));
+    printf("%p",ptr);
+    for(int i=0; i<5; i++){
+        printf("vector [%d] %d\n",i, vec[i]);
+        printf("%d\n",*(ptr++));
     }
     return 0;
 }
